c57c0793de982f92bca8e1add1344fa239768853
Main Admin <farsuller2020@gmail.com>
2020-08-21 11:07:07 +0800

    added libs, designing, serializing

--- app/src/main/java/com/viewer/movieviewer/MovieViewerActivity.kt
+++ app/src/main/java/com/viewer/movieviewer/MovieViewerActivity.kt
@@ -3,9 +3,9 @@ package com.viewer.movieviewer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MovieViewerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_movie_viewer)
     }
 }
---
+++ app/src/main/java/com/viewer/movieviewer/vo/MovieDetails.kt
@@ -0,0 +1,45 @@
+package com.viewer.movieviewer.vo
+
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetails(
+    @SerializedName("advisory_rating")
+    val advisoryRating: String,
+    @SerializedName("average_rating")
+    val averageRating: Any,
+    @SerializedName("canonical_title")
+    val canonicalTitle: String,
+    val cast: List<String>,
+    val genre: String,
+    @SerializedName("has_schedules")
+    val hasSchedules: Int,
+    @SerializedName("is_featured")
+    val isFeatured: Int,
+    @SerializedName("is_inactive")
+    val isInactive: Int,
+    @SerializedName("is_showing")
+    val isShowing: Int,
+    @SerializedName("link_name")
+    val linkName: String,
+    @SerializedName("movie_id")
+    val movieId: String,
+    val order: Int,
+    val poster: String,
+    @SerializedName("poster_landscape")
+    val posterLandscape: String,
+    @SerializedName("release_date")
+    val releaseDate: String,
+    @SerializedName("runtime_mins")
+    val runtimeMins: String,
+    val synopsis: String,
+    val theater: String,
+    @SerializedName("total_reviews")
+    val totalReviews: Any,
+    val trailer: String,
+    val variants: List<String>,
+    @SerializedName("watch_list")
+    val watchList: Boolean,
+    @SerializedName("your_rating")
+    val yourRating: Int
+)
